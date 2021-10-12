5189fb1675e57524b4794ab955d33522c01bbfcd
SnO2WMaN <me@sno2wman.net>
2021-10-12 18:11:17 +0900

    cspellupd

--- src/upserter/upserter.module.ts
+++ src/upserter/upserter.module.ts
@@ -1,6 +1,6 @@
 import {Module} from '@nestjs/common';
 
-import {UpserterController} from './upserter.contoller';
+import {UpserterController} from './upserter.controller';
 import {UpserterService} from './upserter.service';
 
 import {MeiliSearchModule} from '~/meilisearch/meilisearch.module';
