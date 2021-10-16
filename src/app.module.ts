5248de91a433aa82bb4552280243d44fa92fc354
SnO2WMaN <me@sno2wman.net>
2021-10-17 07:29:07 +0900

    fxfxfxfxfx

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import {GraphQLFederationModule} from '@nestjs/graphql';
 
 import {AppConfig} from './app.config';
 import {HealthModule} from './health/health.module';
-import {HealthModule} from './health/health.module';
 import {SearchResolverModule} from './search/search.resolver.module';
 
 @Module({
